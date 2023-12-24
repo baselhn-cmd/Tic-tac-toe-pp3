34399d230fb07a0c5ab1374afed710fd2ee241d7
baselhn-cmd <>
2023-12-24 09:29:41 +0000

    Add name methode and symbol method

--- run.py
+++ run.py
@@ -9,3 +9,20 @@ class Player:
     def __init(self):
         self.name = ""
         self.symbol = ""
+
+    def choose_name(self):
+        while True:
+            name = input("Enter your name (letters only): ")
+            if name.isalpha():
+                self.name = name
+                break
+            print("Invalid name. Try again.")
+
+
+    def choose_symbol(self):
+        while True:
+            symbol = input(f"{self.name}, choose your symbol (letters only): ")
+            if symbol.isalpha() and len(symbol) == 1:
+                self.symbol = symbol.upper()
+                break
+            print("Invalid symbol. Try again.")
