b3780083e77d67dbd1cf305a60f58865f0e9eed5
baselhn-cmd <>
2023-12-26 20:16:21 +0000

    the game stopped workingand i tryng to fix it

--- run.py
+++ run.py
@@ -1,164 +1,147 @@
 import os
 
-def clear_creen():
+# Function to clear the console screen
+def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+# Player class for handling player details
 class Player:
-  def __init__(self):
-    self.name = ""
-    self.symbol = ""
+    def __init__(self):
+        self.name = ""
+        self.symbol = ""
 
-  def choose_name(self):
-    while True:
-      name = input("Enter your name (letters only): ")
-      if name.isalpha():
-        self.name = name
-        break
-      print("Invalid name. Try again.")
-  def choose_symbol(self):
-    while True:
-      symbol = input(f"{self.name}, choose your symbol (letters only): ")
-      if symbol.isalpha() and len(symbol) == 1:
-        self.symbol = symbol.upper()
-        break
-      print("Invalid symbol. Try again.")
+    def choose_name(self):
+        while True:
+            name = input("Enter your name (letters only): ")
+            if name.isalpha():
+                self.name = name
+                break
+            print("Invalid name. Try again.")
 
+    def choose_symbol(self):
+        while True:
+            symbol = input(f"{self.name}, choose your symbol (letters only): ")
+            if symbol.isalpha() and len(symbol) == 1:
+                self.symbol = symbol.upper()
+                break
+            print("Invalid symbol. Try again.")
 
+# Menu class for displaying menus
+class Menu:
+    def display_main_menu(self):
+        print("Welcome to my Tic-Tac-Toe game!")
+        print("1. Start a new game")
+        print("2. Quit")
+        choice = input("Enter your choice (1 or 2): ")
+        return choice
 
-class menu:
-    def displsy_main_menu(self):
-      print("Welcome to my x-O game!")
-      print("1. Start a new game")
-      print("2. Quit")
-      choice = input("Enter your choice (1 or 2): ")
-      return choice
     def display_endgame_menu(self):
-      choice = input("Game Over!\n1. Play again\n2. Quit\nEnter your choice (1 or 2): ")
-      return choice
+        choice = input("Game Over!\n1. Play again\n2. Quit\nEnter your choice (1 or 2): ")
+        return choice
 
+# Board class for managing the game board
+class Board:
+    def __init__(self):
+        self.board = [str(i) for i in range(1, 10)]
 
+    def display_board(self):
+        for i in range(0, 9, 3):
+            print("|".join(self.board[i:i + 3]))
+            if i < 6:
+                print("-" * 5)
 
-
-
-class board:
-   def __init__(self):
-       self.board = [str(i) for i in range(1,10)]
-      
-
-  def display_board(self):
-    """
-    Display the board
-    """
-    for i in range(0,9, 3):
-      print("|".join (self.board[i:i+3]))
-      if i < 6:
-        print("-"*5)
-   
-    
-  def update_board(self, choice, symbol):
-    if self.is_valid_move(choice):
-      self.board[choice-1] = symbol
-      return True
-    return False
-
-  def is_valid_move(self, choice):
-     return self.board[choice-1].isdigit()
-
-  def reseset_board(self):
-    self.board = [str(i) for i in range(1,10)]
-
-class Game:
-  def __init__(self):
-    self.players = [Player(), Player()]
-    self.board = board()
-    self.menu = menu()
-    self.current_player_index = 0
-
-  def start_game(self):
-    choice = self.menu.displsy_main_menu()
-    if choice == "1":
-      self.setup_players()
-      self.play_game()
-    else:
-      self.quit_game()
-
-  def setup_players(self):
-    for number, player in enumerate(self.players, start=1):
-      print(f"Player {number}, enter your details:")
-      player.choose_name()
-      player.choose_symbol() 
-      clear_creen()
-      # print("_"*20)
-  
-
-  def play_game(self):
-    while True:
-      self.play_turn()
-      if self.check_win() or self.check_draw():
-        choice = self.menu.display_endgame_menu()
-        if choice == "1":
-          self.restart_game()
-        else:
-          self.quit_game()
-          break
-      
-
-
-    
-  def restart_game(self):
-      self.board.reseset_board()
-      self.current_player_index = 0
-      self.play_game()
-      
-      
-  
-  def check_win(self):
-    win_combainations = [
-      [0,1,2], [3,4,5], [6,7,8], # horizontal
-      [0,3,6], [1,4,7], [2,5,8], # vertical
-      [0,4,8], [2,4,6] # diagonal
-    ]
-
-    for combination in win_combainations:
-      if self.board.board[combination[0]] == self.board.board[combination[1]] == \
-self.board.board[combination[2]] != "":
-        print(f"{self.players[self.current_player_index].name} wins!")
-        return True
-    return False
-
-  def check_draw(self):
-    for cell in range(len(self.board.board)):
-      if self.board.board[cell].isdigit():
+    def update_board(self, choice, symbol):
+        if self.is_valid_move(choice):
+            self.board[choice - 1] = symbol
+            return True
         return False
-    print("Draw!")
-    return True
 
+    def is_valid_move(self, choice):
+        return self.board[choice - 1].isdigit()
 
-  
-  def play_turn(self):
-    player = self.players[self.current_player_index]
-    self.board.display_board()
-    print(f"{player.name}, choose your move: ({player.symbol})")
-    while True:
-      try:
-          cell_choice = int(input("Enter your choice (1-9): "))
-          if 1<= cell_choice <=9 and self.board.update_board(cell_choice, player.symbol):
-           break
-          else:
-           print("Invalid move. Try again.")
-      except ValueError:
-        print("Invalid move. Try again.")
-    self.switch_player()
+    def reset_board(self):
+        self.board = [str(i) for i in range(1, 10)]
 
-  def switch_player(self):
-      self.current_player_index = 1 - self.current_player_index
+# Game class for managing the game logic
+class Game:
+    def __init__(self):
+        self.players = [Player(), Player()]
+        self.board = Board()
+        self.menu = Menu()
+        self.current_player_index = 0
 
-  
-  def quit_game(self):
-    print("Thanks for playing!")
-    exit()  
+    def start_game(self):
+        choice = self.menu.display_main_menu()
+        if choice == "1":
+            self.setup_players()
+            self.play_game()
+        else:
+            self.quit_game()
 
+    def setup_players(self):
+        for number, player in enumerate(self.players, start=1):
+            print(f"Player {number}, enter your details:")
+            player.choose_name()
+            player.choose_symbol()
+            clear_screen()
 
+    def play_game(self):
+        while True:
+            self.play_turn()
+            if self.check_win() or self.check_draw():
+                choice = self.menu.display_endgame_menu()
+                if choice == "1":
+                    self.restart_game()
+                else:
+                    self.quit_game()
+                    break
 
+    def restart_game(self):
+        self.board.reset_board()
+        self.current_player_index = 0
+        self.play_game()
+
+    def check_win(self):
+        win_combinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # vertical
+            [0, 4, 8], [2, 4, 6]  # diagonal
+        ]
+
+        for combination in win_combinations:
+            if self.board.board[combination[0]] == self.board.board[combination[1]] == \
+                    self.board.board[combination[2]] != "":
+                print(f"{self.players[self.current_player_index].name} wins!")
+                return True
+        return False
+
+    def check_draw(self):
+        for cell in range(len(self.board.board)):
+            if self.board.board[cell].isdigit():
+                return False
+        print("Draw!")
+        return True
+
+    def play_turn(self):
+        player = self.players[self.current_player_index]
+        self.board.display_board()
+        print(f"{player.name}, choose your move: ({player.symbol})")
+        while True:
+            try:
+                cell_choice = int(input("Enter your choice (1-9): "))
+                if 1 <= cell_choice <= 9 and self.board.update_board(cell_choice, player.symbol):
+                    break
+                else:
+                    print("Invalid move. Try again.")
+            except ValueError:
+                print("Invalid move. Try again.")
+        self.switch_player()
+
+    def switch_player(self):
+        self.current_player_index = 1 - self.current_player_index
+
+    def quit_game(self):
+        print("Thanks for playing")
 
 game = Game()
 game.start_game()
